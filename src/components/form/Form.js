fb4f71d1d51c4e8691d3a1dcaead08c36e1b6718
Marge <marge.consunji@gmail.com>
2022-06-28 16:40:12 +0800

    Make reusable form components

---
+++ src/components/form/Form.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const FormGroup = styled.div`
+  display: block;
+`;
+
+export const FormLabel = styled.label`
+  font-weight: normal;
+  font-size: 0.9rem;
+  margin-bottom: 0.4rem;
+  display: block;
+`;
+
+export const FormInput = styled.input`
+  display: block;
+  padding: 0.6rem 0.8rem;
+  border: 2px solid var(--black);
+  border-radius: 6px;
+  font-weight: bold;
+  margin-bottom: 0.8rem;
+`;
