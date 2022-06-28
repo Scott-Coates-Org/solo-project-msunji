c58ed861308f931b8a68637acadadcd67e74ef70
Marge <marge.consunji@gmail.com>
2022-06-28 16:58:40 +0800

    Add more form styling
    - Add fullwidth variant that allows input to fill container

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const FormGroup = styled.div`
-  display: block;
+  display: ${(props) => (props.fullWidth ? 'flex' : 'block')};
+  flex-direction: ${(props) => (props.fullWidth ? 'column' : null)};
 `;
 
 export const FormLabel = styled.label`
@@ -14,7 +15,7 @@ export const FormLabel = styled.label`
 export const FormInput = styled.input`
   display: block;
   padding: 0.6rem 0.8rem;
-  border: 2px solid var(--black);
+  border: 2px solid grey;
   border-radius: 6px;
   font-weight: bold;
   margin-bottom: 0.8rem;
