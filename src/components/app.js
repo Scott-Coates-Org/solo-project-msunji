f4579f97fc6d05cc6466fc83b8205ee250161bb2
Marge <marge.consunji@gmail.com>
2022-06-28 12:15:49 +0800

    Remove Bootstrap stylesheet import

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from 'components/home';
 import { AuthProvider, useAuth } from 'components/user/auth';
 import Login from 'components/user/login';
 import Logout from 'components/user/logout';
 import { firebase } from 'firebase/client';
 import { createBrowserHistory } from 'history';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
-import { Route, Router, Switch } from "react-router-dom";
+import { Route, Router, Switch } from 'react-router-dom';
 import store from 'redux/store';
 import { getData, getDataSuccess } from 'redux/user';
 import ErrorBoundary from 'components/error-boundary';
@@ -37,10 +36,10 @@ function App() {
     dispatch(getData());
   }, []);
 
-  const storeUserData = user => {
+  const storeUserData = (user) => {
     const providerData = user.providerData[0];
 
-    const userData = { ...providerData, uid: user.uid, };
+    const userData = { ...providerData, uid: user.uid };
 
     dispatch(getDataSuccess(userData));
   };
@@ -50,14 +49,24 @@ function App() {
       <AuthProvider onLogin={storeUserData}>
         <Router history={history}>
           <Switch>
-            <Route path="/login" render={(routeProps) => <Login {...routeProps} {...props} firebase={firebase} />} />
-            <Route path="/logout" render={(routeProps) => <Logout {...routeProps} {...props} firebase={firebase} />} />
+            <Route
+              path="/login"
+              render={(routeProps) => (
+                <Login {...routeProps} {...props} firebase={firebase} />
+              )}
+            />
+            <Route
+              path="/logout"
+              render={(routeProps) => (
+                <Logout {...routeProps} {...props} firebase={firebase} />
+              )}
+            />
 
             {/* this must be on the bottom */}
             <ProtectedRoute path="/" component={Home} {...props} />
           </Switch>
         </Router>
-      </AuthProvider >
+      </AuthProvider>
     </ErrorBoundary>
   );
 
@@ -70,11 +79,13 @@ export default AppWithRedux;
 // https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#1-protecting-a-route-in-a-react-router-dom-app
 const ProtectedRoute = ({ component, ...args }) => {
   const WrappedComponent = withAuthenticationRequired(component, {
-    onRedirecting: () => "resuming session…",
+    onRedirecting: () => 'resuming session…',
   });
 
   const retVal = (
-    <Route render={(routeProps) => <WrappedComponent {...routeProps} {...args} />} />
+    <Route
+      render={(routeProps) => <WrappedComponent {...routeProps} {...args} />}
+    />
   );
 
   return retVal;
@@ -92,7 +103,6 @@ function withAuthenticationRequired(Component, options) {
       loginOptions = {},
     } = options;
 
-
     useEffect(async () => {
       let isAuthorized = false;
 
@@ -108,16 +118,16 @@ function withAuthenticationRequired(Component, options) {
             },
           };
 
-          history.push('/login', opts)
+          history.push('/login', opts);
         }
       }
-
     }, [history, isAuthenticated, loginOptions, returnTo]);
 
     return isAuthenticated ? <Component {...props} /> : onRedirecting();
   };
 }
 
-const defaultReturnTo = () => `${window.location.pathname}${window.location.search}`;
+const defaultReturnTo = () =>
+  `${window.location.pathname}${window.location.search}`;
 
 const defaultOnRedirecting = () => <></>;
