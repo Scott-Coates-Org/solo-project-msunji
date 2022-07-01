17ba86077c9e131bd30d375edc88496316b04ff9
Marge <marge.consunji@gmail.com>
2022-07-01 12:20:44 +0800

    Merge breakpoint and colour data and rename
    - Get breakpoint value and move it to this file.

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    white: '#fff',
+    mustard: '#ffc300',
+    black: '#030A04',
+    blue: '#55DDE0',
+    blueGreyLight: '#E1ECF4',
+    blueDark: '#2F4858',
+    grey: '#848d93',
+    orange: '#F26419',
+    satin: '#CE6A85',
+  },
+  breakpoints: {
+    sm: '700px',
+  },
+};
+
+export default theme;
