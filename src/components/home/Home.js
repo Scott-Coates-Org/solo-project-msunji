ce7d24cdcd1019278fbf26de77e7fd914f16cba3
Marge <marge.consunji@gmail.com>
2022-07-01 13:13:11 +0800

    Set base styling, add link

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,35 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from 'components/button/Button';
+import Container from 'components/container/Container';
+import Shapes from './Shapes';
+
+const StyledHome = styled.main`
+  min-height: 100vh;
+  display: flex;
+`;
+
+const HomeContainer = styled(Container)`
+  position: relative;
+`;
+
 const Home = () => {
-  return <main>Test</main>;
+  return (
+    <StyledHome>
+      <HomeContainer>
+        <Shapes />
+        <div>
+          <h1>Kulay</h1>
+          <p>Text goes here</p>
+          <Link to="/generate">
+            <Button type="orange" size="auto" shape="pill">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </HomeContainer>
+    </StyledHome>
+  );
 };
 
 export default Home;
