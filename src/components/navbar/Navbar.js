aec4d84a6c05decd0965e680e134b7a067dbf0a2
Marge <marge.consunji@gmail.com>
2022-07-06 16:38:18 +0800

    Make basic nav component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import Button from 'components/button/Button';
+const Navbar = () => {
+  return (
+    <nav>
+      <div>logo</div>
+      <p>this should differ depending on route</p>
+    </nav>
+  );
+};
+
+export default Navbar;
