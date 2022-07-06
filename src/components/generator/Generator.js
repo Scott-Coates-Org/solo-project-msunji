dc01ce671fd22d0e44fc6b66feff42a6d1957cc7
Marge <marge.consunji@gmail.com>
2022-07-06 15:05:43 +0800

    Get RGB values from image data array
    - Iterate through RGBA values in array to produce an array of objects containing R, G, and B values

--- src/components/generator/Generator.js
+++ src/components/generator/Generator.js
@@ -1,22 +1,39 @@
 import { useForm } from 'react-hook-form';
 
+const parseToRGB = (dataArr) => {
+  // getImageData returns an array of RGBA values. This makes a new array that consists of only RGB values
+  let rgbVals = [];
+  for (let i = 0; i < dataArr.length; i += 4) {
+    rgbVals.push({
+      r: dataArr[i],
+      g: dataArr[i + 1],
+      b: dataArr[i + 2],
+    });
+  }
+  console.log('parsed rgb', rgbVals);
+  return rgbVals;
+};
+
 const parseURLtoImg = ({ url }) => {
-  let colourArr;
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
-  const img = new Image();
-  img.crossOrigin = 'anonymous';
-  img.src = url;
-  img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0, canvas.width, canvas.height);
-    colourArr = context.getImageData(0, 0, canvas.width, canvas.height).data;
-    console.log(colourArr);
-  };
-  console.log(colourArr);
-  // return console.log('url', url);
-  console.log(img);
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = url;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      let colourArr = context.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      ).data;
+      resolve(colourArr);
+    };
+  });
 };
 
 const Generator = () => {
@@ -26,7 +43,13 @@ const Generator = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => parseURLtoImg(data);
+
+  const loadData = async (data) => {
+    let imgData = await parseURLtoImg(data);
+    console.log('moto data', imgData);
+    parseToRGB(imgData);
+  };
+  const onSubmit = (data) => loadData(data);
   return (
     <div>
       <h1>Test Generator</h1>
