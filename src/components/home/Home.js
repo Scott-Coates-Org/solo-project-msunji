eda5e76e94db40258061de2bea832f9a6b4cdb59
Marge <marge.consunji@gmail.com>
2022-07-06 17:23:40 +0800

    Adjust layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,40 +3,48 @@ import styled from 'styled-components';
 import Button from 'components/button/Button';
 import Shapes from './Shapes';
 
-const StyledHome = styled.main`
+const StyledHome = styled.section`
   min-height: 100vh;
+  background: #fffff2;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #fffff2;
+  position: relative;
 `;
 
 const HomeContainer = styled.div`
-  position: relative;
+  height: fit-content;
   display: flex;
+  flex-direction: column;
 
   .home-text {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    width: 100%;
+    max-width: 900px;
+
+    padding: 0 2rem;
+    h1 {
+      padding-top: 0;
+    }
     z-index: 10;
+    p {
+      font-size: 2rem;
+    }
   }
 `;
 
 const HomeButton = styled(Button)`
   font-size: 1.7rem;
+  border: 2px solid black;
 `;
 
 const Home = () => {
   return (
     <StyledHome>
       <HomeContainer>
-        <Shapes />
         <div className="home-text">
           <h1>Kulay</h1>
           <p>
-            <strong>Kulay</strong> makes colour palettes that consist of
-            dominant colours on a website.
+            <strong>Kulay</strong> is a simple tool for making colour palettes.
           </p>
           <Link to="/generator">
             <HomeButton type="orange" width="auto" shape="pill" size="lg">
@@ -44,6 +52,7 @@ const Home = () => {
             </HomeButton>
           </Link>
         </div>
+        <Shapes />
       </HomeContainer>
     </StyledHome>
   );
