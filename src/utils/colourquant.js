e97631790530eb8e9d83446d6af78eecab7ed6bd
Marge <marge.consunji@gmail.com>
2022-07-06 15:05:34 +0800

    Return as Promise

--- src/utils/colourquant.js
+++ src/utils/colourquant.js
@@ -1,18 +1,21 @@
 const parseURLtoImg = ({ url }) => {
-  let colourArr;
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
-  const img = new Image();
-  img.crossOrigin = 'anonymous';
-  img.src = url;
-  img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0, canvas.width, canvas.height);
-    colourArr = context.getImageData(0, 0, canvas.width, canvas.height).data;
-    console.log(colourArr);
-  };
-  console.log(colourArr);
-  // return console.log('url', url);
-  console.log(img);
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = url;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      let colourArr = context.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      ).data;
+      resolve(colourArr);
+    };
+  });
 };
