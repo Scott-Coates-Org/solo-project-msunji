7bd37878de1f777f396aaf4977171fbe291c35c9
Marge <marge.consunji@gmail.com>
2022-07-11 13:07:30 +0800

    Calculate range for each channel in the image

--- src/components/generator/Generator.js
+++ src/components/generator/Generator.js
@@ -1,7 +1,10 @@
 import { useForm } from 'react-hook-form';
+import MainLayout from 'components/layouts/MainLayout';
+import Container from 'components/container/Container';
 
 const parseToRGB = (dataArr) => {
-  // getImageData returns an array of RGBA values. This makes a new array that consists of only RGB values
+  // getImageData returns an array of RGBA values. We want to remove alpha values
+  // This function makes a new array that consists of only RGB values
   let rgbVals = [];
   for (let i = 0; i < dataArr.length; i += 4) {
     rgbVals.push({
@@ -12,6 +15,44 @@ const parseToRGB = (dataArr) => {
   }
   console.log('parsed rgb', rgbVals);
   return rgbVals;
+};
+
+const getRange = (dataArr) => {
+  // Get the upper- and lower-bounds for each channel
+  // Calculate lower bounds;
+  const getMin = (dataArr, channel) => {
+    return dataArr.reduce((acc, pixel) => {
+      return acc < pixel[channel] ? acc : pixel[channel];
+    });
+  };
+  // Calculate upper bounds;
+  const getMax = (dataArr, channel) => {
+    return dataArr.reduce((acc, pixel) => {
+      return acc > pixel[channel] ? acc : pixel[channel];
+    });
+  };
+  // Lower bounds for each channel
+  const rMin = getMin(dataArr, 'r');
+  const gMin = getMin(dataArr, 'g');
+  const bMin = getMin(dataArr, 'b');
+  // Upper bounds for each channel
+  const rMax = getMax(dataArr, 'r');
+  const gMax = getMax(dataArr, 'g');
+  const bMax = getMax(dataArr, 'b');
+
+  console.log('rmin', rMin);
+  console.log('rmax', rMax);
+  console.log('gmin', gMin);
+  console.log('gmax', gMax);
+  console.log('bmin', bMin);
+  console.log('bmax', bMax);
+
+  // Calculate range per channel
+  const rRange = rMax - rMin;
+  const gRange = gMax - gMin;
+  const bRange = bMax - bMin;
+
+  console.log(rRange, gRange, bRange);
 };
 
 const parseURLtoImg = ({ url }) => {
@@ -47,17 +88,20 @@ const Generator = () => {
   const loadData = async (data) => {
     let imgData = await parseURLtoImg(data);
     console.log('moto data', imgData);
-    parseToRGB(imgData);
+    let imgDataChannels = parseToRGB(imgData);
+    getRange(imgDataChannels);
   };
   const onSubmit = (data) => loadData(data);
   return (
-    <div>
-      <h1>Test Generator</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('url', { required: true })} />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <MainLayout>
+      <Container>
+        <p>Test Generator</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('url', { required: true })} />
+          <button type="submit">Submit</button>
+        </form>
+      </Container>
+    </MainLayout>
   );
 };
 
