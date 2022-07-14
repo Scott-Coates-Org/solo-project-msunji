b9fa5b6de7e7f74230bc456b3c5b6364e77179c9
Marge <marge.consunji@gmail.com>
2022-07-14 13:42:49 +0800

    Remove widget

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import { reducer as user } from './user';
-import { reducer as widget } from './widget';
 
 const reducer = combineReducers({
     user,
-    widget,
 });
 
 const store = configureStore({
