de15e52b5fb8df47afa3746fc23c42f7db1097f2
Marge <marge.consunji@gmail.com>
2022-07-21 14:31:59 +0800

    Add copy, style header text

--- src/components/generator/Generator.js
+++ src/components/generator/Generator.js
@@ -8,15 +8,28 @@ import Container from 'components/container/Container';
 import { FormInput } from 'components/form/Form';
 import PropagateLoader from 'react-spinners/PropagateLoader';
 
+const GeneratorText = styled.div`
+  text-align: center;
+  margin: 0 auto;
+
+  p {
+    max-width: 60ch;
+    margin: 0 auto;
+  }
+`;
+
 const FormSection = styled.section`
   width: 100%;
   height: 100%;
-  background: var(--mustard);
   padding: 3rem 0;
 
   h1 {
-    font-size: 1.5rem;
+    font-size: 4rem;
     text-align: center;
+    color: var(--mustard);
+    background: linear-gradient(150deg, #9414c7, #f63a5c, #fff585);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
   }
 `;
 
@@ -54,7 +67,11 @@ const Generator = () => {
     <MainLayout>
       <FormSection>
         <Container>
-          <h1>Generate a Palette</h1>
+          <GeneratorText>
+            <h1>Generate a Palette</h1>
+            <p>Using <b>Kulay</b> is pretty straightforward. Add a URL to the form shown below, hit submit, and wait for the magic to happen! ✨</p>
+          </GeneratorText>
+
           <form onSubmit={handleSubmit(onSubmit)}>
             <FormInput {...register('url', { required: true })} />
             {}
